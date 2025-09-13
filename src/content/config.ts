cc256ffc9b22845673f40df55c062f53e37504fb
ezzabuzaid <ezza@kortext.com>
2025-09-13 17:05:46 +0800

    add page description

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,56 +1,57 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 // biome-ignore lint/style/noNamespaceImport: This only affects SSG so this is fine to use here.
-import * as Icons from 'lucide-preact';
+import * as Icons from "lucide-preact";
 
 const IconKeys = Object.keys(Icons) as [string, ...Array<string>];
 
-
 const posts = defineCollection({
-	type: 'content',
-	schema: z
-		.object({
-			archived: z.boolean().optional().default(false),
-			description: z.string(),
-			draft: z.boolean().optional().default(false),
-			featured: z.number(),
-			icon: z.enum(IconKeys).default('Newspaper'),
-			lastModifiedAt: z.coerce.date().optional(),
-			publishedAt: z.coerce.date(),
-			title: z.string(),
-		})
-		.strict(),
+  type: "content",
+  schema: z
+    .object({
+      archived: z.boolean().optional().default(false),
+      description: z.string(),
+      draft: z.boolean().optional().default(false),
+      featured: z.number(),
+      icon: z.enum(IconKeys).default("Newspaper"),
+      lastModifiedAt: z.coerce.date().optional(),
+      publishedAt: z.coerce.date(),
+      title: z.string(),
+    })
+    .strict(),
 });
 
 const projects = defineCollection({
-	type: 'data',
-	schema: z
-		.object({
-			$schema: z.string().optional(),
-			date: z.coerce.date(),
-			icon: z.enum(IconKeys),
-			title: z.string(),
-			description: z.string().optional(),
-			url: z.string().optional(),
-			status: z.enum(['idea', 'development', 'maintenance', 'archived']).optional(),
-		})
-		.strict(),
+  type: "data",
+  schema: z
+    .object({
+      $schema: z.string().optional(),
+      date: z.coerce.date(),
+      icon: z.enum(IconKeys),
+      title: z.string(),
+      description: z.string().optional(),
+      url: z.string().optional(),
+      status: z
+        .enum(["idea", "development", "maintenance", "production", "archived"])
+        .optional(),
+    })
+    .strict(),
 });
 
 const socialLinks = defineCollection({
-	type: 'data',
-	schema: z
-		.object({
-			$schema: z.string().optional(),
-			icon: z.string(),
-			name: z.string(),
-			showLabel: z.coerce.boolean().optional().default(false),
-			url: z.string().url(),
-		})
-		.strict(),
+  type: "data",
+  schema: z
+    .object({
+      $schema: z.string().optional(),
+      icon: z.string(),
+      name: z.string(),
+      showLabel: z.coerce.boolean().optional().default(false),
+      url: z.string().url(),
+    })
+    .strict(),
 });
 
 export const collections = {
-	posts,
-	projects,
-	socialLinks,
+  posts,
+  projects,
+  socialLinks,
 };
